50f84397516734885b20975cd3f6b3f871651b37
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-07-29 19:17:47 +0800

    Practiced hoisting and TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* 
+
 function calcAge(birthYear) {
     const age = 2037 - birthYear;
 
@@ -39,3 +41,58 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
+
+*/
+
+// Variable Hoisting
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Function Hoisting
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+// Lesson: The only way you can do hoisting for functions is to make the function a function declaration
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExpr = function (a, b) {
+    return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Example 
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
+
+// JS Best Practices 
+
+// 1. Dont use var to declare variables and use const to declare variables and let if you need to change the variable later 
+
+//2. In order to write clean code, delcare your variables at the top of each scope, it will make the code look better
+
+//3. Always all of your functions first and use them only after the declaration -> This apples to all types of functions even if function declarations are hoisted to write cleaner code 
+
+var x = 1; 
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
