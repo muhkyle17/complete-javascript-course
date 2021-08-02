f5ee3e9dbb0abf48b7b87e5f619633c166307b29
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-08-02 15:45:27 +0800

    Nullish coalescing operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -46,7 +46,16 @@ const restaurant = {
   
 };
 
+//////////////////////////////////
+// The Nullish Coalescing Operator
 
+// restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish value: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
 
 /*
 //////////////////////////////
