e513995869aeb9aa08189b2a2eece1ccffea01fa
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-08-03 13:02:54 +0800

    Learnd about enhanced object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 12 + 12,
+  },
+};
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -11,28 +28,16 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+  // ES6 enhanced object literals
+  openingHours,
 
-  order: function(starterIndex, mainIndex) {
+  order (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ( {starterIndex =1 , mainIndex = 0, time = '20:00', address} ) {
+  orderDelivery ( {starterIndex =1 , mainIndex = 0, time = '20:00', address} ) {
     console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliver to ${address} at ${time}`);
-  },
-  
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
   },
 
   orderPasta: function (ing1, ing2, ing3) {
@@ -46,6 +51,13 @@ const restaurant = {
   
 };
 
+
+
+/*
+
+///////////////////////////////////////
+// For Of Loop
+
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) console.log(item);
@@ -56,7 +68,6 @@ for (const [i, el] of menu.entries()) {
 
 // console.log([...menu.entries()]);
 
-/*
 
 ///////////////////////////////////////
 // Coding Challenge #1
