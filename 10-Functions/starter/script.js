a11e55e190037c77afc9b592570901766d7f2708
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-08-09 10:42:38 +0800

    Learned How Passing Arguments Work

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* 
 const bookings = [];
 
 const createBooking = function(
@@ -27,4 +28,40 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 
-createBooking('LH123', undefined, 1000);
+createBooking('LH123', undefined, 1000); // This is a simple trick to skip the value you want to set as default 
+
+*/
+
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 23424092423
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if (passenger.passport === 23424092423) {
+        alert('Checked In')
+    } else {
+        alert('Wrong passport!');
+    }
+}
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// This is the name as doing this except we're passing it into a function 
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 10000000000);
+}
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+// Javascript does not have pass by reference only by value 
