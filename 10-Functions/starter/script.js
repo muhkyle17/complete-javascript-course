1496d65e500e781f8d3f8143daedcf227517a8bb
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-08-10 13:02:27 +0800

    Made a few examples for closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -367,3 +367,45 @@ console.dir(booker);
 // It's like a person never losses its connection to their hometown 
 // A closure is like a backpack that a function carries around wherever it goes. This backpack has all the variables that were present in the environment where the function was created 
 // We do not have to manually create closures, this is something that JavaScript does automatically 
+
+// More about Closures (Examples)
+
+// Example 1 
+let f;
+
+const g = function() {
+  const a = 23;
+  f = function() {
+    console.log(a * 2);
+  };
+};
+
+const h = function() {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  }
+}
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function to h 
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n , wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function(){
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
