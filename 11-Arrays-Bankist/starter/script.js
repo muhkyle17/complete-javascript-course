ec72a26832f98bcccdb0557549a52ce759e96880
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-08-12 15:07:18 +0800

    Finished with coding challenge #2

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -325,15 +325,13 @@ for (const mov of movements) {
 }
 console.log(withdrawalsFor);
 
-*/
-
 ///////////////////////////////////////
 // Reduce Method
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 console.log(movements);
 
-// acc - accumulator is like a snowball that accumulates all of the lements
+// acc - accumulator is like a snowball that accumulates all of the elements
 // const balance = movements.reduce(function (acc, cur, i, arr) {
 //   console.log(`Iteration ${i + 1}: ${acc}`)
 //   return acc + cur 
@@ -356,3 +354,65 @@ const max = movements.reduce(function(acc,  mov) {
   }
 }, movements[0])
 console.log(max);
+
+*/
+
+///////////////////////////////////////
+// Coding Challnge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+*/
+
+const calcAverageHumanAge = function (ages) {
+  let humanAge = 0;
+  const humanAgeArr = [];
+
+  ages.forEach(function(mov) {
+    if (mov <= 2) {
+      humanAge = 2 * mov;
+      humanAgeArr.push(humanAge);
+    } else if (mov > 2) {
+      humanAge = 16 + mov * 4;
+      humanAgeArr.push(humanAge);
+    };
+  });
+
+  console.log(humanAgeArr)
+
+  const adults = humanAgeArr.filter(function (mov) {
+    return mov >= 18
+  })
+
+  console.log(adults);
+
+  // const average = adults.reduce(function(acc, age, i, arr){
+  //   return acc + age / arr.length
+  // }, 0)
+
+  //OR
+  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+  //OR 
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length, 0
+  );
+  
+  console.log(average);
+}
+
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
