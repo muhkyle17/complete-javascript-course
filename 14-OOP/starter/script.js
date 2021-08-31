ceaee2db369e0e203ac4b8d6714a3ea80d634835
Mikyle Reyes <reyesmikyle17@gmail.com>
2021-08-31 12:42:07 +0800

    Learned more indepth about Prototype Inheritance on Built in Objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -50,3 +50,26 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName')); // true
 console.log(jonas.hasOwnProperty('species')); // false because it's not inside the property itself but has access to it
+
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 4, 5, 4, 2, 2, 1, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
